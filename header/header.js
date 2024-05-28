24ffb06fcec031256c4618ebd7894a934ec2a7d2
Santiago <slopez@scl.edu.gt>
2024-05-28 09:28:53 -0600

    tranbajo

---
+++ header/header.js
@@ -0,0 +1,10 @@
+let header_santi = document.querySelector(".header inicio");
+
+header_santi.innerHTML=`
+<header class="inicio">
+    <div>Bloque 1</div>
+    <div>bloque 2</div>
+    <div>bloque 3</div>
+</header>
+
+`;
---
+++ section/section.js
@@ -0,0 +1,9 @@
+let section_informacion = document.querySelector(".section1");
+
+section_informacion.innerHTML=`
+<section class="info">
+    <div>Bloque 1</div>
+    <div>bloque 2</div>
+    <div>bloque 3</div> 
+</section>
+`;
